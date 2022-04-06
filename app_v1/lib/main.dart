dd2c1b5204c85f971c9e521a3a449b4d8440e049
Elevenkicker <team@elevenkicker.de>
2022-04-06 09:15:38 +0200

    basic

--- app_v1/lib/main.dart
+++ app_v1/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen/home_screen.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +16,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Elevenkicker"),
+        backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
+        centerTitle: true,
+        title: const Text(
+          "Elevenkicker",
+          style: TextStyle(fontFamily: "LeagueGothic", fontSize: 45),
+        ),
+      ),
+      body: const HomeScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_rounded),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_soccer_rounded),
+            label: "Games",
+          ),
+        ],
       ),
     );
   }
