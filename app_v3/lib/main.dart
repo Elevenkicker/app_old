ad3388a7edbddd30dbb2511979c7b17741181532
Elevenkicker <team@elevenkicker.de>
2022-04-14 09:50:21 +0200

    Add Team

--- app_v3/lib/main.dart
+++ app_v3/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'provider/2. team/teams_provider.dart';
 import 'screens/1. HomeScreen/Widgets/2. FollowedTeams/add_favourite_team.dart';
 import 'screens/1. HomeScreen/home_screen.dart';
 import 'screens/2. GamesScreen/games_screen.dart';
@@ -35,41 +37,44 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        HomeScreen.screenName: (context) => const HomeScreen(),
-        TeamScreen.screenName: (context) => const TeamScreen(),
-        AddFavouriteTeam.screenName: (context) => const AddFavouriteTeam(),
-      },
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
-          centerTitle: true,
-          title: const Text("Elevenkicker",
-              style: TextStyle(fontFamily: "LeagueGothic", fontSize: 45)),
-        ),
-        body: const HomeScreen(),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedPageIndex,
-          onTap: _selectPage,
-          backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white54,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search_rounded),
-              label: "Search",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.sports_soccer_rounded),
-              label: "Games",
-            ),
-          ],
+    return ChangeNotifierProvider(
+      create: (context) => TeamsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeScreen.screenName: (context) => const HomeScreen(),
+          TeamScreen.screenName: (context) => const TeamScreen(),
+          AddFavouriteTeam.screenName: (context) => const AddFavouriteTeam(),
+        },
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
+            centerTitle: true,
+            title: const Text("Elevenkicker",
+                style: TextStyle(fontFamily: "LeagueGothic", fontSize: 45)),
+          ),
+          body: const HomeScreen(),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedPageIndex,
+            onTap: _selectPage,
+            backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white54,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search_rounded),
+                label: "Search",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.sports_soccer_rounded),
+                label: "Games",
+              ),
+            ],
+          ),
         ),
       ),
     );
--- app_v3/lib/screens/1. HomeScreen/home_screen.dart
+++ app_v3/lib/screens/1. HomeScreen/home_screen.dart
@@ -1,7 +1,5 @@
-import 'package:app_v3/provider/2.%20team/teams_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:provider/provider.dart';
 import 'Widgets/2. FollowedTeams/1. followed_teams_field.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,25 +14,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => TeamsProvider(),
-      child: Scaffold(
-        backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: const [
-                  Gap(20),
-                  // FollowedPlayers(),
-                  Gap(20),
-                  FollowedTeamsField(),
-                  Gap(20),
-                  // FollowedLeagues(),
-                  Gap(20),
-                ],
-              ),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(16, 38, 102, 1),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: const [
+                Gap(20),
+                // FollowedPlayers(),
+                Gap(20),
+                FollowedTeamsField(),
+                Gap(20),
+                // FollowedLeagues(),
+                Gap(20),
+              ],
             ),
           ),
         ),
